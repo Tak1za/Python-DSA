15c27bd033f21be1f6a4998053d31b7e6c39fe54
varun <varungupta2015135@gmail.com>
2020-08-06 17:24:52 +0530

    Added infinite monkey theorem and square root

---
+++ infinite_monkey_theorem.py
@@ -0,0 +1,33 @@
+import random
+
+def generate(strlen):
+  alphabet = "abcdefghijklmnopqrstuvwxyz "
+  res = ""
+  for i in range(strlen):
+    res = res + alphabet[random.randrange(27)]
+  return res
+
+def score(goal, testString):
+  numSame = 0
+  for i in range(len(goal)):
+    if goal[i]==testString[i]:
+      numSame = numSame + 1
+  return numSame/len(goal) * 100
+
+def main():
+  goalString = "methinks it is like a weasel"
+  newString = generate(28)
+  iterations = 0
+  bestScore = 0
+  bestString = ""
+  while score(goalString, newString) < 100:
+    newString = generate(28)
+    if score(goalString, newString) > bestScore :
+      bestScore = score(goalString, newString)
+      bestString = newString
+    iterations = iterations + 1
+    if iterations % 2000 == 0:
+      print("Best score is %s with the string generated as (%s)" % (str(bestScore), bestString))
+  print("String matched after %d tries." % iterations)
+
+main()
---
+++ square_root.py
@@ -0,0 +1,7 @@
+def sqrRoot(num):
+  guess = num/2
+  for i in range(30):
+    guess = (1/2)*(guess+(num/guess))
+  return "%.2f" % guess
+
+print(sqrRoot(347832))
