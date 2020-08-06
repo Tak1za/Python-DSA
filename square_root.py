01c939ae3e370222facf0743658ef36a74c1afef
varun <varungupta2015135@gmail.com>
2020-08-06 18:48:22 +0530

    Added formatting

--- square_root.py
+++ square_root.py
@@ -1,7 +1,8 @@
 def sqrRoot(num):
-  guess = num/2
-  for i in range(30):
-    guess = (1/2)*(guess+(num/guess))
-  return "%.2f" % guess
+    guess = num/2
+    for i in range(30):
+        guess = (1/2)*(guess+(num/guess))
+    return "%.2f" % guess
+
 
 print(sqrRoot(347832))
