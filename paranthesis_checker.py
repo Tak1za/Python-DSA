c7c475303f45fc3a9436eb5408f6f781c0a70e86
Varun Gupta <varungupta2015135@gmail.com>
2020-08-09 16:59:02 +0530

    Modified parenthesis checker to balance all kinds of paranthesis

--- paranthesis_checker.py
+++ paranthesis_checker.py
@@ -23,3 +23,32 @@ print(parChecker('(()'))
 print(parChecker('(())()()())))))'))
 print(parChecker('(())()()())))))((((('))
 print(parChecker('((a))(b*d)()())))))((((('))
+
+print("-------------------------------------")
+
+def parCheckerAll(symbolString):
+    s = Stack()
+
+    for i in symbolString :
+        if i in '[{(':
+            s.push(i)
+        elif i in ']})':
+            if s.isEmpty():
+                return "Unbalanced"
+            elif matches(i, s.peek()):
+                s.pop()
+            else:
+                return "Unbalanced"
+        
+    if s.isEmpty():
+        return "Balanced"
+    else:
+        return "Unbalanced"
+
+def matches(symbol, top):
+    openers = "[{("
+    closers = "]})"
+    return openers.index(top) == closers.index(symbol)
+
+print(parCheckerAll('{({([][])}())}'))
+print(parCheckerAll('[{()]'))
